439432493b000471e612bf055dfaa226382cdceb
Your Name <you@example.com>
2023-05-17 19:36:32 +0900

    環境作成

---
+++ dev/my_todo/list_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+app_name = "App"
+
+urlpatterns = [path('', views.home_view, name='home'),]
