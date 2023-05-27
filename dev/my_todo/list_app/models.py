9f4ac005bdc7b059e56f1d4770c35deba5d2f194
katori-senko <katori515@gmail.com>
2023-05-27 18:20:48 +0900

    モデル定義

--- dev/my_todo/list_app/models.py
+++ dev/my_todo/list_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class ProjectLists(models.Model):
+    name = models.CharField(max_length=256)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TodoItems(models.Model):
+    todo_list = models.ForeignKey(ProjectLists, on_delete=models.CASCADE)
+    task = models.CharField(max_length=256)
+    in_detail = models.CharField(max_length=1024)
+    rating_value = models.FloatField(validators=[MaxValueValidator(100.00)])
+    completed = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task
